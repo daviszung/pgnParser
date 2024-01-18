a3451330ca4c3f7b352f5156338d8e274aac1573
daviszung <daviszung@gmail.com>
2024-01-18 11:23:47 -0500

    w

--- src/validateFormat.ts
+++ src/validateFormat.ts
@@ -1,15 +1,19 @@
 
 type formatOrder = "number" | "whiteMove" | "blackMove"
 
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+type File = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h"
+
 export function validateFormat(pgn: string) {
+
+    // separate the pgn into parts
+    const parts = pgn.split(" ");
 
     // the format will expect a number, then white's move, then black's move, and then back to a number
     let order: formatOrder = "number"
 
     // track what set of moves we are on
     let count = 1
-
-    const parts = pgn.split(" ");
 
     for (const i of parts) {
         switch (order) {
@@ -25,11 +29,15 @@ export function validateFormat(pgn: string) {
         
             case "whiteMove":
 
+                disectMove(i, "w")
+
                 order = "blackMove"
 
                 break;
 
             case "blackMove":
+
+                disectMove(i, "b")
 
                 order = "number"
 
@@ -42,5 +50,54 @@ export function validateFormat(pgn: string) {
 
 
     return true;
+
+}
+
+type MoveAnatomy = {
+    color: "w" | "b"
+    piece: "P" | "N" | "B" | "R" | "Q" | "K" | null
+    pieceSpecifier: Rank | File | null
+    rank: Rank | null 
+    file: File | null
+    captures: boolean
+    check: boolean
+    checkmate: boolean
+    special: false | "shortCastle" | "longCastle"
+    valid: boolean
+}
+
+const pieces = ["N", "B", "R", "Q", "K"]
+
+function disectMove(move: string, color: "w" | "b") {
+
+    const moveAnatomy: MoveAnatomy = {
+        color: color,
+        piece: null, 
+        pieceSpecifier: null,
+        rank: null,
+        file: null,
+        captures: false,
+        check: false,
+        checkmate: false,
+        special: false,
+        valid: false,
+    }
+
+    // special move
+    if (move === "O-O") {
+        moveAnatomy["special"] = "shortCastle"
+        moveAnatomy["valid"] = true
+        return moveAnatomy
+    } else if (move === "O-O-O") {
+        moveAnatomy["special"] = "longCastle"
+        moveAnatomy["valid"] = true
+        return moveAnatomy
+    }
+
+    for (let i = 0; i < move.length; i++) {
+
+    }
+
+    return moveAnatomy
 
 }
